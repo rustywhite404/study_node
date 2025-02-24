64413fece67f280e72ec2b4093dff17bc2fed0f9
rustywhite404 <rustywhite404@gmail.com>
2025-02-24 22:23:33 +0900

    [feat] POST 요청 API 구현

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const app = express();
+const app = express(); 
+
+app.use(express.urlencoded({extended: true})) 
 
 app.listen(8080, function() {
     console.log('listening on 8080')
@@ -13,6 +15,11 @@ app.get('/pet', function(요청, 응답) {
     응답.send('펫용품 사시오')
   })
 
-app.get('/beauty', function(요청, 응답){
-    응답.send('뷰티용품 사시오')
+app.get('/write', function(요청, 응답){
+    응답.sendFile(__dirname + '/write.html')
+})
+
+app.post('/add', function(요청, 응답){
+    console.log(요청.body);
+    응답.send('전송 완료');
 })
