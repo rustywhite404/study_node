1e7735274661c1c267929aa7ec9c47dbb4d4e43d
rustywhite404 <rustywhite404@gmail.com>
2025-02-28 21:13:39 +0900

    [feat] ELS로 화면에서 데이터 보기

--- server.js
+++ server.js
@@ -3,14 +3,9 @@ const MongoClient = require('mongodb').MongoClient;
 const app = express(); 
 let db;
 
-const uri = "mongodb+srv://rustywhite404:비밀번호호@cluster0.qw0l4.mongodb.net/myDB?retryWrites=true&w=majority&appName=Cluster0";
-
-
+const uri = "mongodb+srv://rustywhite404:%40Rkgus6628@cluster0.qw0l4.mongodb.net/myDB?retryWrites=true&w=majority&appName=Cluster0";
+app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true})) 
-
-// app.listen(8080, function() {
-//     console.log('listening on 8080')
-// })
 
 app.get('/', function(요청, 응답) {
     응답.sendFile(__dirname + '/index.html')
@@ -23,11 +18,7 @@ app.get('/pet', function(요청, 응답) {
 app.get('/write', function(요청, 응답){
     응답.sendFile(__dirname + '/write.html')
 })
-
-// app.post('/add', function(요청, 응답){
-//     console.log(요청.body);
-//     응답.send('전송 완료');
-// }) 
+ 
 
 app.post('/add', async (req, res) => {
     try {
@@ -39,7 +30,19 @@ app.post('/add', async (req, res) => {
         console.error("❌ 데이터 저장 실패:", error);
         res.status(500).send("데이터 저장 실패!");
     }
+}); 
+
+app.get('/list', async function(req, res) {
+    try {
+        const result = await db.collection('post').find().toArray(); // 데이터를 가져옴
+        console.log("✅ 조회된 데이터:", result);
+        res.render('list.ejs', { posts: result }); // 데이터를 EJS로 전달
+    } catch (error) {
+        console.error("❌ 데이터 조회 실패:", error);
+        res.status(500).send("데이터 조회 실패!");
+    }
 });
+
 
 async function connectDB() {
     try {
